c57a36b910b0bd9f50a7baa349d55b1fda8ee71f
gamaa_000 <gamaa_000@10.0.5.151>
2017-04-07 19:11:08 -0500

    Merge origin/master
    Conflicts:
    	src/Node/Node.java

--- CORBA/src/Multicast/LeaderObj.java
+++ CORBA/src/Multicast/LeaderObj.java
@@ -4,11 +4,14 @@
  * and open the template in the editor.
  */
 package Multicast;
-
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 /**
  *
  * @author gamaa_000
  */
+
 public class LeaderObj extends dms.RemoteLeaderPOA{
     @Override
     public void update( String owner , String[] list){
