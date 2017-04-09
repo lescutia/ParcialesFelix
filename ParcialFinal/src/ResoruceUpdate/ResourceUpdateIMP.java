6be39cf2166cbb29b84c3bcfda5e6eb05eee94ec
gamaa <gamaa@10.0.5.138>
2017-04-09 13:45:17 -0500

    .

---
+++ ParcialFinal/src/ResoruceUpdate/ResourceUpdateIMP.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ResoruceUpdate;
+import java.util.ArrayList;
+/**
+ *
+ * @author gamaa
+ */
+public class ResourceUpdateIMP implements ResourceUpdate{
+    @Override
+    public void update(ArrayList list){
+        System.out.println();
+    } 
+}
