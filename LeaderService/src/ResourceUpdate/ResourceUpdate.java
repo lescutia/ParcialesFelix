e2ed29e473961622a455d61644bd6fe260f46de0
gamaa <gamaa@10.0.5.138>
2017-04-11 21:56:29 -0500

    Merge origin/master
    Conflicts:
    	ParcialFinal/config.txt

---
+++ LeaderService/src/ResourceUpdate/ResourceUpdate.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ResourceUpdate;
+import java.rmi.*;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author gamaa
+ */
+public interface ResourceUpdate extends Remote{
+    
+    /*
+    *   @Param fileList is the list of available resources.
+    */
+    public void update(String owner, ArrayList<String> fileList) throws RemoteException;
+    public ArrayList<ArrayList<String>> getTable() throws RemoteException;
+}
